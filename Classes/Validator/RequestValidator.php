c245c72f1a6661efa52ae8b04fc1da362ee3032b
Paulo Paes <pauloleospaes@gmail.com>
2021-11-03 21:07:59 -0300

    Fixing DB connection and added modeling of API

--- Classes/Validator/RequestValidator.php
+++ Classes/Validator/RequestValidator.php
@@ -27,7 +27,6 @@ class RequestValidator
         //echo '6';
         $retorno = utf8_encode(ConstantesGenericasUtil::MSG_ERRO_TIPO_ROTA);
         
-        $this->request['metodo'] = 'POST';
         if(in_array($this->request['metodo'], ConstantesGenericasUtil::TIPO_REQUEST, true)){
             $retorno = $this->direcionarRequest();
         }
